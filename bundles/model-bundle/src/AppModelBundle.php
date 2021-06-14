c31e19ce1b1283d29c75e4de4acb773fc03a6d48
Antonio J. García Lagar <ajgarcia@c17.net>
2021-06-14 13:35:17 +0200

    Add model-bundle

---
+++ bundles/model-bundle/src/AppModelBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundle\ModelBundle;
+
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppModelBundle extends Bundle
+{
+}
